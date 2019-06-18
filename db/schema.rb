30ada42df4cf5eb24c71642d7bc569ec6b94e9fa
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-06-18 22:25:05 +0300

    Add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_181740) do
+ActiveRecord::Schema.define(version: 2019_06_18_192301) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 2019_06_18_181740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["number"], name: "index_repairs_on_number", unique: true
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
